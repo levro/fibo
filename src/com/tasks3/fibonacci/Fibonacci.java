85a6dc9f2d8b342a326c9356409a082b6dabde95
levro <levro@ukr.net>
2017-03-06 11:15:40 +0200

    1st

---
+++ src/com/tasks3/fibonacci/Fibonacci.java
@@ -0,0 +1,11 @@
+package com.tasks3.fibonacci;
+
+public class Fibonacci {
+
+	public long getNumber(int position){
+		long f = 0;
+
+		return f;
+	}
+
+}
---
+++ src/com/tasks3/fibonacci/fibo.java
@@ -0,0 +1,10 @@
+package com.tasks3.fibonacci;
+
+public class fibo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
