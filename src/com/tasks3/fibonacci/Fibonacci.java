1921be409deceda65571dae1f85e1e7ef5028abe
levro <levro@ukr.net>
2017-03-06 11:49:44 +0200

    v1

--- src/com/tasks3/fibonacci/Fibonacci.java
+++ src/com/tasks3/fibonacci/Fibonacci.java
@@ -2,10 +2,17 @@ package com.tasks3.fibonacci;
 
 public class Fibonacci {
 
-	public long getNumber(int position){
-		long f = 0;
+	public static long getNumber(int position){
+		if (position == 0)
+			return 0;
+		else if (position <= 2)
+			return 1;
+		else return getNumber(position - 1) + getNumber(position - 2); 
+	}
 
-		return f;
+	public static void main(String[] args) {
+//		for (int i=0; i<11; i++){
+		System.out.println(getNumber(35));
 	}
 
 }
